9bf2bbe906a224891a44dff92a449ee7f95f4fae
dlace51 <142068671+dlace51@users.noreply.github.com>
2024-11-02 20:56:28 -0500

    updated for pythonanywhere

--- myshop/settings.py
+++ myshop/settings.py
@@ -123,7 +123,7 @@ if not DEBUG:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR,"static")
     MEDIA_ROOT = MEDIA_DIR
-    MEDIA_URL = '/media/'
+    MEDIA_URL = 'media/'
 
 else:
     MEDIA_URL = '/media/'
@@ -137,6 +137,7 @@ CART_SESSION_ID = 'cart'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
